c9e19c65f878dc6f212b16b780b5c2c9c8e1a4ee
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-03-30 12:32:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "23631217"))
 API_HASH = environ.get("API_HASH", "567c6df308dc6901790309499f729d12")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/b40ec933c5cfe1c4f92d0-1c4eb4466dbd880cc5.jpg https://graph.org/file/afb26904467e6d288066d-e47db06938c1b2426d.jpg https://graph.org/file/93ab4f396b7e1702c0f1e-9240161b97e8d7ad9b.jpg https://graph.org/file/784c2dbbbcc0a5b8f8481-bedb60c5a57f254efc.jpg https://graph.org/file/6342f9ae8571659b764d2-adb46a36274462e7f5.jpg https://graph.org/file/58588618cd2547af98235-c024050c84364be078.jpg https://graph.org/file/413bdcc12fffa83f2e44f-d55d528ec54053892f.jpg https://graph.org/file/4bb4161b9b29ca7a9a68b-09129a323354e7ca94.jpg https://graph.org/file/afb26904467e6d288066d-e47db06938c1b2426d.jpg  https://graph.org/file/1c9861f4bff950b2ccb6b-4a686c9d9244369aeb.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://envs.sh/AXV.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6139759254').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "MZStorFXrobot") # without @
 PORT = environ.get("PORT", "8080")
@@ -73,7 +73,7 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://mrnfilesharing.blogspot.com/2025/01/redirecting-to-your-link-code-credit.html") # For More Information Check Video On Yt - @Tech_VJ
