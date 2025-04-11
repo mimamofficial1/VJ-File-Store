95a87865c880c979ee38a76b34f00a283273a855
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-04-11 19:15:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,18 +22,6 @@ from urllib.parse import quote_plus
 from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
 
-async def is_subscribed(bot, query, channel):
-    btn = []
-    for id in channel:
-        chat = await bot.get_chat(int(id))
-        try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
-        except Exception as e:
-            pass
-    return btn
-
 BATCH_FILES = {}
 
 # Don't Remove Credit Tg - @VJ_Botz
