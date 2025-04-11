84ed0e65f3540b841b8a84151e435b42b59d4105
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-04-11 20:07:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,10 +11,10 @@ from Script import script
 from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
-from pyrogram.errors import
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, *
+from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.types import *
 from utils import verify_user, check_token, check_verification, get_token
-from config import AUTH_CHANNEL, *
+from config import *
 import re
 import json
 import base64
